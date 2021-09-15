ec396e02009120e43a0bc8f8cca572e8814ec382
BuildTools <unconfigured@null.spigotmc.org>
2021-09-15 19:25:58 -0400

    Initial commit

--- src/main/java/me/badvait/enhanceddebugmenu/mixin/DebugHudMixin.java
+++ src/main/java/me/badvait/enhanceddebugmenu/mixin/DebugHudMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
-@Mixin(DebugHud.class)
+@Mixin(DebugHud.class) // test
 public abstract class DebugHudMixin {
 
     @Shadow
